62b19edb25c301788c8474ca617352802934b290
huguet10@gmail.com <huguet10@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-11-20 19:59:02 +0000

    Se añade configuración de Spring

---
+++ trunk/PracticaQuizz/quizz.model/src/main/java/quizz/movember/model/config/SpringModelConfiguration.java
@@ -0,0 +1,10 @@
+package quizz.movember.model.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "quizz.movember.model" })
+public class SpringModelConfiguration {
+
+}
