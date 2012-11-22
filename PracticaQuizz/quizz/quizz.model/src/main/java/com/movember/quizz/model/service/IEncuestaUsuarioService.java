99b871dc988311af4cc92b7520921868ed989fce
huguet10@gmail.com <huguet10@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-11-22 19:49:03 +0000

    Interfaces de servicios

---
+++ PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/IEncuestaUsuarioService.java
@@ -0,0 +1,7 @@
+package com.movember.quizz.model.service;
+
+import com.movember.quizz.model.bean.EncuestaUsuario;
+
+public interface IEncuestaUsuarioService extends IModelService<EncuestaUsuario> {
+
+}
---
+++ PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/IRespuestaUsuarioService.java
@@ -0,0 +1,7 @@
+package com.movember.quizz.model.service;
+
+import com.movember.quizz.model.bean.RespuestaUsuario;
+
+public interface IRespuestaUsuarioService extends IModelService<RespuestaUsuario> {
+
+}
