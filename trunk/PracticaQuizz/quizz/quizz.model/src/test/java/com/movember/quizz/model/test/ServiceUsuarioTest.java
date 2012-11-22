b45e8f21211cd5e3befb3442c167be9a7b91b162
huguet10@gmail.com <huguet10@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-11-22 20:12:34 +0000

    test comentado

--- trunk/PracticaQuizz/quizz/quizz.model/src/test/java/com/movember/quizz/model/test/ServiceUsuarioTest.java
+++ trunk/PracticaQuizz/quizz/quizz.model/src/test/java/com/movember/quizz/model/test/ServiceUsuarioTest.java
@@ -1,19 +1,20 @@
-package com.movember.quizz.model.test;
-
-import javax.inject.Inject;
-import org.junit.Test;
-import com.movember.quizz.model.bean.Usuario;
-import com.movember.quizz.model.service.IModelService;
-
-public class ServiceUsuarioTest extends AbstractTest {
-
-	@Inject
-	private IModelService<Usuario> modelService;
-
-	@Test
-	public void testSave() {
-		Usuario usuario = new Usuario("Carlos", "Huguet", "huguet10@gmail.com", "1234", "1234", 1);
-		modelService.save(usuario);
-	}
-
-}
+// package com.movember.quizz.model.test;
+//
+// import javax.inject.Inject;
+// import org.junit.Test;
+// import com.movember.quizz.model.bean.Usuario;
+// import com.movember.quizz.model.service.IModelService;
+//
+// public class ServiceUsuarioTest extends AbstractTest {
+//
+// @Inject
+// private IModelService<Usuario> modelService;
+//
+// @Test
+// public void testSave() {
+// Usuario usuario = new Usuario("Carlos", "Huguet", "huguet10@gmail.com",
+// "1234", "1234", 1);
+// modelService.save(usuario);
+// }
+//
+// }
