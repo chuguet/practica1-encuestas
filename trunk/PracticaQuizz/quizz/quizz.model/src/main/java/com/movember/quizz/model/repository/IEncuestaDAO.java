01afa1ab9c3a08f4804622263b6f0b645389e462
huguet10@gmail.com <huguet10@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-11-22 19:32:45 +0000

    Interfaces de repositorios

---
+++ trunk/PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/repository/IEncuestaDAO.java
@@ -0,0 +1,7 @@
+package com.movember.quizz.model.repository;
+
+import com.movember.quizz.model.bean.Encuesta;
+
+public interface IEncuestaDAO extends IRepositoryDAO<Encuesta> {
+
+}
---
+++ trunk/PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/repository/IRespuestaUsuario.java
@@ -0,0 +1,7 @@
+package com.movember.quizz.model.repository;
+
+import com.movember.quizz.model.bean.RespuestaUsuario;
+
+public interface IRespuestaUsuario extends IRepositoryDAO<RespuestaUsuario> {
+
+}
