c2752c10d36016657b4e2579fbf62f7b5169ab08
david.llamazares.juarez@gmail.com <david.llamazares.juarez@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-12-01 19:39:58 +0000

    Insertado iBatis y arreglado toda la parte de encuestas

---
+++ PracticaQuizz/quizz/quizz.controller/src/main/java/com/movember/quizz/controller/dto/PreguntaDTO.java
@@ -0,0 +1,81 @@
+package com.movember.quizz.controller.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.movember.quizz.model.bean.Pregunta;
+import com.movember.quizz.model.bean.Respuesta;
+
+public class PreguntaDTO extends AbstractDTO {
+	private String title;
+	private boolean isFolder;
+	private String key;
+	private List<RespuestaDTO> children = new ArrayList<RespuestaDTO>();
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean getIsFolder() {
+		return isFolder;
+	}
+
+	public void setIsFolder(boolean isFolder) {
+		this.isFolder = isFolder;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public List<RespuestaDTO> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<RespuestaDTO> children) {
+		this.children = children;
+	}
+
+	@Override
+	public void toRest(Object object) {
+		Pregunta pregunta = (Pregunta) object;
+		if (pregunta.getId() != null) {
+			this.key = "p" + pregunta.getId().toString();
+			this.setId(pregunta.getId());
+		}
+		this.title = pregunta.getPregunta();
+		this.isFolder = true;
+		if (pregunta.getRespuestas() != null && pregunta.getRespuestas().size() > 0) {
+			for (Respuesta respuesta : pregunta.getRespuestas()) {
+				RespuestaDTO respuestaDTO = new RespuestaDTO();
+				respuestaDTO.toRest(respuesta);
+				this.children.add(respuestaDTO);
+			}
+		}
+	}
+
+	@Override
+	public void toBusiness(Object object) {
+		Pregunta pregunta = (Pregunta) object;
+		if (this.key.indexOf('p') != -1) {
+			pregunta.setId(Integer.parseInt(this.key.replace("p", "")));
+		}
+		pregunta.setPregunta(this.title);
+
+		if (this.children != null && this.children.size() > 0) {
+			for (RespuestaDTO respuestaDTO : this.children) {
+				Respuesta respuesta = new Respuesta();
+				respuesta.setId_pregunta(pregunta.getId());
+				respuestaDTO.toBusiness(respuesta);
+				pregunta.getRespuestas().add(respuesta);
+			}
+		}
+	}
+}
