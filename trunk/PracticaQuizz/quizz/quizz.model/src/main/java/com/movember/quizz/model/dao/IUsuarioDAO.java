553b862de6261ab65508a7f83ba5287b320cf3ec
david.llamazares.juarez@gmail.com <david.llamazares.juarez@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-12-04 21:29:25 +0000

    Insertado spring security con CustomAuthorized

--- trunk/PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/dao/IUsuarioDAO.java
+++ trunk/PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/dao/IUsuarioDAO.java
@@ -1,7 +1,10 @@
 package com.movember.quizz.model.dao;
 
+import java.sql.SQLException;
 import com.movember.quizz.model.bean.Usuario;
 
 public interface IUsuarioDAO extends IRepositoryDAO<Usuario> {
+
+	Usuario selectByUser(String usuarioNombre) throws SQLException;
 
 }
