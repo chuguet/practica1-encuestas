5645d5de43855a220bc55fe8d43cc1cbfed00ba5
huguet10@gmail.com <huguet10@gmail.com@1e92182c-c115-6150-589a-5e5e19edf1a8>
2012-12-16 16:04:52 +0000

    Se añade Javadoc

--- PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/EncuestaService.java
+++ PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/EncuestaService.java
@@ -11,41 +11,66 @@ import com.movember.quizz.model.bean.Pregunta;
 import com.movember.quizz.model.dao.IEncuestaDAO;
 import com.movember.quizz.model.exception.AppException;
 
+
+/**
+ * The Class EncuestaService.
+ */
 @Service
 class EncuestaService implements IEncuestaService {
 
+	/** The encuesta dao. */
 	@Inject
 	private IEncuestaDAO encuestaDAO;
+
+	/** The pregunta service. */
 	@Inject
 	private IPreguntaService preguntaService;
+
+	/** The respuesta service. */
 	@Inject
 	private IRespuestaService respuestaService;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#insert(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void insert(Encuesta encuesta) throws AppException {
 		try {
 			encuestaDAO.insert(encuesta);
-			if (encuesta.getPreguntas() != null && encuesta.getPreguntas().size() > 0) {
+			if (encuesta.getPreguntas() != null
+					&& encuesta.getPreguntas().size() > 0) {
 				for (Pregunta pregunta : encuesta.getPreguntas()) {
 					pregunta.setId_encuesta(encuesta.getId());
 					this.preguntaService.insert(pregunta);
 				}
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al insertar la encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al insertar la encuesta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#update(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void update(Encuesta encuesta) throws AppException {
 		try {
 			encuestaDAO.update(encuesta);
-			if (encuesta.getPreguntas() != null && encuesta.getPreguntas().size() > 0) {
-				List<Pregunta> preguntasAntiguas = this.preguntaService.recuperarDeEncuesta(encuesta.getId());
+			if (encuesta.getPreguntas() != null
+					&& encuesta.getPreguntas().size() > 0) {
+				List<Pregunta> preguntasAntiguas = this.preguntaService
+						.recuperarDeEncuesta(encuesta.getId());
 				for (Pregunta pregunta : encuesta.getPreguntas()) {
 					if (pregunta.getId() != null) {
 						this.preguntaService.update(pregunta);
-					}
-					else {
+					} else {
 						this.preguntaService.insert(pregunta);
 					}
 				}
@@ -57,71 +82,109 @@ class EncuestaService implements IEncuestaService {
 					}
 				}
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al actualizar la encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al actualizar la encuesta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#delete(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void delete(Encuesta encuesta) throws AppException {
 		try {
-			if (encuesta.getPreguntas() != null && encuesta.getPreguntas().size() > 0) {
+			if (encuesta.getPreguntas() != null
+					&& encuesta.getPreguntas().size() > 0) {
 				for (Pregunta pregunta : encuesta.getPreguntas()) {
 					this.preguntaService.delete(pregunta);
 				}
 			}
 			encuestaDAO.delete(encuesta.getId());
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al eliminar la encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al eliminar la encuesta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#retrieve(java.lang.Integer)
+	 */
 	public Encuesta retrieve(Integer id) throws AppException {
 		Encuesta encuesta = null;
 		try {
 			encuesta = encuestaDAO.retrieve(id);
 			if (encuesta != null) {
-				encuesta.setPreguntas(this.preguntaService.recuperarDeEncuesta(id));
+				encuesta.setPreguntas(this.preguntaService
+						.recuperarDeEncuesta(id));
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar la encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar la encuesta");
 		}
 		return encuesta;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.movember.quizz.model.service.IService#selectAll()
+	 */
 	public List<Encuesta> selectAll() throws AppException {
 		List<Encuesta> encuestas = null;
 		try {
 			encuestas = encuestaDAO.selectAll();
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar todas las encuestas");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar todas las encuestas");
 		}
 		return encuestas;
 	}
 
-	public List<Encuesta> find(ParametrosEncuesta parametrosEncuesta) throws AppException {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IEncuestaService#find(com.movember.quizz
+	 * .model.bean.ParametrosEncuesta)
+	 */
+	public List<Encuesta> find(ParametrosEncuesta parametrosEncuesta)
+			throws AppException {
 		List<Encuesta> encuestas = null;
 		try {
 			encuestas = encuestaDAO.find(parametrosEncuesta);
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar un listado de encuestas");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar un listado de encuestas");
 		}
 		return encuestas;
 	}
 
-	public void contestar(EncuestaContestada encuestaContestada) throws AppException {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IEncuestaService#contestar(com.movember
+	 * .quizz.model.bean.EncuestaContestada)
+	 */
+	public void contestar(EncuestaContestada encuestaContestada)
+			throws AppException {
 		try {
 			encuestaDAO.contestar(encuestaContestada);
-			for (Integer idRespuesta : encuestaContestada.getIdRespuestasContestadas()) {
-				respuestaService.contestar(encuestaContestada.getId(), idRespuesta);
+			for (Integer idRespuesta : encuestaContestada
+					.getIdRespuestasContestadas()) {
+				respuestaService.contestar(encuestaContestada.getId(),
+						idRespuesta);
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al contestar una encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al contestar una encuesta");
 		}
 	}
 }
--- PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/PreguntaService.java
+++ PracticaQuizz/quizz/quizz.model/src/main/java/com/movember/quizz/model/service/PreguntaService.java
@@ -9,39 +9,62 @@ import com.movember.quizz.model.bean.Respuesta;
 import com.movember.quizz.model.dao.IPreguntaDAO;
 import com.movember.quizz.model.exception.AppException;
 
+
+/**
+ * The Class PreguntaService.
+ */
 @Service
 class PreguntaService implements IPreguntaService {
 
+	/** The pregunta dao. */
 	@Inject
 	private IPreguntaDAO preguntaDAO;
+
+	/** The respuesta service. */
 	@Inject
 	private IRespuestaService respuestaService;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#insert(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void insert(Pregunta pregunta) throws AppException {
 		try {
 			preguntaDAO.insert(pregunta);
-			if (pregunta.getRespuestas() != null && pregunta.getRespuestas().size() > 0) {
+			if (pregunta.getRespuestas() != null
+					&& pregunta.getRespuestas().size() > 0) {
 				for (Respuesta respuesta : pregunta.getRespuestas()) {
 					respuesta.setId_pregunta(pregunta.getId());
 					this.respuestaService.insert(respuesta);
 				}
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al insertar una pregunta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al insertar una pregunta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#update(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void update(Pregunta pregunta) throws AppException {
 		try {
 			preguntaDAO.update(pregunta);
-			if (pregunta.getRespuestas() != null && pregunta.getRespuestas().size() > 0) {
-				List<Respuesta> respuestasAntiguas = this.respuestaService.recuperarDePregunta(pregunta.getId());
+			if (pregunta.getRespuestas() != null
+					&& pregunta.getRespuestas().size() > 0) {
+				List<Respuesta> respuestasAntiguas = this.respuestaService
+						.recuperarDePregunta(pregunta.getId());
 				for (Respuesta respuesta : pregunta.getRespuestas()) {
 					if (respuesta.getId() != null) {
 						this.respuestaService.update(respuesta);
-					}
-					else {
+					} else {
 						this.respuestaService.insert(respuesta);
 					}
 				}
@@ -53,54 +76,83 @@ class PreguntaService implements IPreguntaService {
 					}
 				}
 			}
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al actualizar una pregunta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al actualizar una pregunta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#delete(com.movember.quizz.model
+	 * .bean.AbstractBean)
+	 */
 	public void delete(Pregunta pregunta) throws AppException {
 		try {
-			if (pregunta.getRespuestas() != null && pregunta.getRespuestas().size() > 0) {
+			if (pregunta.getRespuestas() != null
+					&& pregunta.getRespuestas().size() > 0) {
 				for (Respuesta respuesta : pregunta.getRespuestas()) {
 					this.respuestaService.delete(respuesta);
 				}
 			}
 			preguntaDAO.delete(pregunta.getId());
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al borrar una pregunta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al borrar una pregunta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IService#retrieve(java.lang.Integer)
+	 */
 	public Pregunta retrieve(Integer id) throws AppException {
 		try {
 			return preguntaDAO.retrieve(id);
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar una pregunta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar una pregunta");
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.movember.quizz.model.service.IService#selectAll()
+	 */
 	public List<Pregunta> selectAll() throws AppException {
 		try {
 			return preguntaDAO.selectAll();
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar una pregunta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar una pregunta");
 		}
 	}
 
-	public List<Pregunta> recuperarDeEncuesta(Integer idEncuesta) throws AppException {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.movember.quizz.model.service.IPreguntaService#recuperarDeEncuesta
+	 * (java.lang.Integer)
+	 */
+	public List<Pregunta> recuperarDeEncuesta(Integer idEncuesta)
+			throws AppException {
 		try {
-			List<Pregunta> preguntas = this.preguntaDAO.recuperarDeEncuesta(idEncuesta);
+			List<Pregunta> preguntas = this.preguntaDAO
+					.recuperarDeEncuesta(idEncuesta);
 			for (Pregunta pregunta : preguntas) {
-				pregunta.setRespuestas(this.respuestaService.recuperarDePregunta(pregunta.getId()));
+				pregunta.setRespuestas(this.respuestaService
+						.recuperarDePregunta(pregunta.getId()));
 			}
 			return preguntas;
-		}
-		catch (SQLException e) {
-			throw new AppException("Se ha producido un error al recuperar las preguntas de una encuesta");
+		} catch (SQLException e) {
+			throw new AppException(
+					"Se ha producido un error al recuperar las preguntas de una encuesta");
 		}
 	}
 }
